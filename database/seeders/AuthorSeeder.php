562584013736dcb1323e9407cdd075be95bc9978
Kristine <qristinehzaqarian@gmail.com>
2022-04-08 18:07:20 +0400

    Added migrations, factories, seeder, models

--- database/seeders/AuthorSeeder.php
+++ database/seeders/AuthorSeeder.php
@@ -3,22 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\Author;
-use App\Models\Book;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AuthorSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
-       Book::factory()
+        Author::factory()
             ->count(10)
-            ->hasAttached(Author::factory()->count(2))
             ->create();
     }
 }
